501ff1506f83857c5d02dd5b7037cf0d23504e01
aprilfaithdays <aprilfaith.e@gmail.com>
2020-02-26 11:47:33 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,9 +3,24 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
-
+  
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    redirect_to @student
+  end
+
+  private
+
+  def student_params
+    @student = params.require(:student).permit(:first_name, :last_name)
   end
 
 end
